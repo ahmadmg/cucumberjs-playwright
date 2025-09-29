cbf703e91589557ff543e6772f56118b5424611d
ahmadmg <ahmadmg@gmail.com>
2025-09-29 14:39:17 +0200

    enh

--- cucumber.mjs
+++ cucumber.mjs
@@ -1,23 +1,26 @@
 export default {
-    format:
-        [
-            'html:reports/cucumber-report.html',
-            'json:reports/cucumber-report.json',
-            "allure-cucumberjs/reporter"
-        ],
-    // How the snippet should look
-    formatOptions: {
-        resultsDir: "allure-results",
-        snippetInterface: 'async-await'
-    },
-    parallel: 1,
-    serial: true,
-    // retry: 2,
-    paths: ['tests/features/*.feature'],
-    require: [
-        'tests/step-definitions/*.ts',
-        'tests/support/**/*.ts',
-        'tests/Pages/**/*.ts'
-    ],
-    requireModule: ['ts-node/register'],
-}
+  format: [
+    "html:reports/cucumber-report.html",
+    "json:reports/cucumber-report.json",
+    "allure-cucumberjs/reporter",
+  ],
+  // How the snippet should look
+  formatOptions: {
+    resultsDir: "allure-results",
+    snippetInterface: "async-await",
+    colorsEnabled: true,
+  },
+   parallel: process.env.CI ? 3 : 1,
+  retry: process.env.CI ? 2 : 0,
+  paths: ["tests/features/*.feature"],
+  require: [
+    "tests/step-definitions/*.ts",
+    "tests/support/**/*.ts",
+    "tests/Pages/**/*.ts",
+  ],
+  requireModule: ["ts-node/register"],
+  publishQuiet: true,
+  dryRun: false,
+  failFast: false,
+  tags: process.env.TAGS || 'not @skip'
+};
